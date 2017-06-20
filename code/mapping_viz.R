abaf968790f47b6994e76eb15f6f1e7c554fc7b3
c-roiger <charlotte.roiger@gmail.com>
2017-06-20 11:16:50 -0600

    updating paths

--- code/mapping_viz.R
+++ code/mapping_viz.R
@@ -1,5 +1,5 @@
 # Clean starting environment
-rm(list = ls())
+# rm(list = ls())
 
 # LIBRARIES
 library(foreign)
@@ -9,17 +9,17 @@ library(rbokeh) # Maybe with a map?
 library(dplyr)
 
 # My working directory
-if(file.exists('~/GitHub/neon_data')){
-  repo_location = '~/GitHub/neon_data'
+if(file.exists('~/GitHub/mosquito_intern')){
+  repo_location = '~/GitHub/mosquito_intern'
 }
 if(!exists('repo_location')){stop('Set the location of the neon_data repository.')}
 
 # Functions to pull NEON data
-source(paste(repo_location, 'get_data/retrieve_data_by_product.R', sep='/'))
+# source(paste(repo_location, 'get_data/retrieve_data_by_product.R', sep='/'))
 
 # Get map of NEON domains
-Ddbf<-read.dbf(paste(repo_location, '/get_data/maps/NEON_Domains.dbf', sep='/'))
-Dmap<-readShapePoly(paste(repo_location, '/get_data/maps/NEON_Domains.shp', sep='/'))
+Ddbf<-read.dbf(paste(repo_location, '/resources/spatial files/NEON_Domains.dbf', sep='/'))
+Dmap<-readShapePoly(paste(repo_location, '/resources/spatial files/NEON_Domains.shp', sep='/'))
 crs(Dmap) <- "+proj=utm +units=m +ellps=WGS84"
 
 # Map the data
