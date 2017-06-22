3e4d502533f7ac81ea84d311a711a8d1958311e9
c-roiger <charlotte.roiger@gmail.com>
2017-06-22 15:29:48 -0600

    @klevan updated mpa viz to fit w neon data but having some issues

--- code/mapping_viz.R
+++ code/mapping_viz.R
@@ -12,44 +12,49 @@ library(rgdal) # requires sp, will use proj.4 if installed
 library(ggplot2)
 
 
-# My working directory
-if(file.exists('~/mosquito-intern')){ # Charlotte's path
-  repo_location = '~/mosquito-intern'
-}
-if(file.exists('~/GitHub/mosquito-intern')){
-  repo_location = '~/GitHub/mosquito-intern'
-}
-if(!exists('repo_location')){stop('Set the location of the neon_data repository.')}
+require("rgdal") # requires sp, will use proj.4 if installed
+require("maptools")
+require("ggplot2")
+require("plyr")
+
+
+# # My working directory
+# if(file.exists('~/mosquito-intern')){ # Charlotte's path
+#   repo_location = '~/mosquito-intern'
+# }
+# if(file.exists('~/GitHub/mosquito-intern')){
+#   repo_location = '~/GitHub/mosquito-intern'
+# }
+# if(!exists('repo_location')){stop('Set the location of the neon_data repository.')}
 
 
 
 # Two ways to make a map
 # Way #1: Map the data using maptools & the plot function
-# Get shp files of NEON domains
-Ddbf<-read.dbf(paste(repo_location, '/resources/spatial files/NEON-domain-map/NEON_Domains.dbf', sep='/'))
-Dmap<-readShapePoly(paste(repo_location, '/resources/spatial files/NEON-domain-map/NEON_Domains.shp', sep='/'))
-crs(Dmap) <- "+proj=utm +units=m +ellps=WGS84"
-plot(Dmap,col="#ADA96E",bg='#77BFC7',main='NEON Domains',cex.main=1)
-points(tars.firstdomain$lon2, tars.firstdomain$lat2, pch=21, bg= "blue",cex=1)
-# points(long, lat)
+# # Get shp files of NEON domains
+# Ddbf<-read.dbf(paste(repo_location, '/resources/spatial files/NEON-domain-map/NEON_Domains.dbf', sep='/'))
+# Dmap<-readShapePoly(paste(repo_location, '/resources/spatial files/NEON-domain-map/NEON_Domains.shp', sep='/'))
+# crs(Dmap) <- "+proj=utm +units=m +ellps=WGS84"
+# plot(Dmap,col="#ADA96E",bg='#77BFC7',main='NEON Domains',cex.main=1)
+# points(tars.firstdomain$lon2, tars.firstdomain$lat2, pch=21, bg= "blue",cex=1)
+# # points(long, lat)
 
 # Way #2: Using rgdal, ggplot and aes
 # Read blog post here: https://github.com/tidyverse/ggplot2/wiki/plotting-polygon-shapefiles
-# Read in the file
-utah = readOGR(dsn=paste0(repo_location, '/resources/spatial files/utah-example'), 
-               layer="eco_l3_ut")
+
+
+#Read in file  
+NEONmap = readOGR(dsn=paste0('mosquito-intern/resources/spatial files/NEON-domain-map'), layer = 'NEON_Domains')
 # Manipulate for plotting
-utah@data$id = rownames(utah@data)
-utah.points = fortify(utah, region="id")
-utah.df = join(utah.points, utah@data, by="id")
-utah@data$id = rownames(utah@data)
-utah.points = fortify(utah, region="id")
-utah.df = join(utah.points, utah@data, by="id")
-# make the plot
-ggplot(utah.df) + 
-  aes(long,lat,group=group,fill=LEVEL3_NAM) + 
-  geom_polygon() +
-  geom_path(color="white") +
-  coord_equal() +
-  scale_fill_brewer("Utah Ecoregion")
+NEONmap@data$id = rownames(NEONmap@data)
+NEONmap.points = fortify(NEONmap, region="id")
+NEONmap.df = join(NEONmap.points, NEONmap@data, by="id")
+NEONmap@data$id = rownames(NEONmap@data)
+NEONmap.points = fortify(NEONmap, region="id")
+NEONmap.df = join(NEONmap.points, NEONmap@data, by="id")
+
+# make the plot, allows for multiple df use
+q <- ggplot()+
+  geom_polygon(data = NEONmap.df, aes(long, lat, group=group))+
+  geom_path(data = NEONmap.df, aes(long,lat, group=group), color = 'white')
 
